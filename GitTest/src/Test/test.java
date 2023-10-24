f48489309f05c0a59bd0b693165dafb1491de5c0
rjy <rjy@gamail.com>
2023-10-24 22:37:28 +0900

    된다

--- GitTest/src/Test/test.java
+++ GitTest/src/Test/test.java
@@ -11,7 +11,12 @@ public class test {
 	
 		System.out.println("test");
 	
-	
+		System.out.println("Test입니다.");
+		System.out.println("해보자.");
+		
+		System.out.println("되겠지?");
+		
+		System.out.println("되겠지?");
 	}
 
 }
